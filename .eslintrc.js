9848d7efd026884faebd96166fe565045aa295bd
shaho <toofani@gmail.com>
2021-07-27 12:17:07 +0430

    Add eslint-prettier configuration and absolute path

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended", // Make sure this is always the last element in the array.
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["simple-import-sort", "prettier"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // https://github.com/prettier/eslint-plugin-prettier#options
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "jsx-a11y/accessible-emoji": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // "simple-import-sort/imports": "error",
+    // "simple-import-sort/exports": "error",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+  },
+};
---
+++ src/test/test2/test3/index.tsx
@@ -0,0 +1,13 @@
+import Welcome from "@/test";
+
+export default function Test3() {
+  return (
+    <div>
+      <h1>
+        <Welcome />
+        <br />
+        Test3
+      </h1>
+    </div>
+  );
+}
